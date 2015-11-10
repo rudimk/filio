74598270319a8b8d0acfddd95eb396f93cde7904
Rudraksh MK <rudraksh.mk@gmail.com>
2015-11-10 18:44:21 +0530

    Added Flask-Admin's fileadmin interface.

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, redirect
 from flask.ext.basicauth import BasicAuth
 from creds import username, password
+from flask_admin import Admin
+from flask_admin.contrib.fileadmin import FileAdmin
+import os.path as op
 
+# Flask setup here
 app = Flask(__name__)
+admin = Admin(app, name='Filio', template_mode='bootstrap3')
 
-app.debug = True
+path = op.join(op.dirname(__file__), 'static')
+admin.add_view(FileAdmin(path, '/static/', name='Static Files'))
+
 app.config['BASIC_AUTH_USERNAME'] = username
 app.config['BASIC_AUTH_PASSWORD'] = password
 app.config['BASIC_AUTH_FORCE'] = True
@@ -18,4 +25,4 @@ def secret_view():
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=9200)
+	app.run(host='0.0.0.0', port=9500, debug=True)
